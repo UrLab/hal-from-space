976d6951dae8fdbede1e5339ba04cf7588f26571
Florentin Hennecker <florentin.hennecker@gmail.com>
2014-09-25 19:08:53 +0200

    [add] comments telling us where to go next

--- meteor/server/server.js
+++ meteor/server/server.js
@@ -1,5 +1,7 @@
 Meteor.methods({
 	update : function() {
 		console.log("In update function");
+		// Read from file private/toread
+		// If Sensor({name:"toread"}) already exists, update it, otherwise create it
 	}
 });
