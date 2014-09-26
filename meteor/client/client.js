d9a77c84823da40f1580dff604985d0d828727b8
Florentin Hennecker <florentin.hennecker@gmail.com>
2014-09-26 12:43:34 +0200

    (ref) removed update button and toread file since the db will be fed from a python script and the client auto-updates when the db is updated.

--- meteor/client/client.js
+++ meteor/client/client.js
@@ -1,9 +1,3 @@
 Template.sensors.values = function() {
 	return Sensors.find({name: {$ne: ""}}, {sort:{name:1}});
 }
-
-Template.updateButton.events = {
-	'click #update' : function(event) {
-		Meteor.call("update");
-	}
-}
