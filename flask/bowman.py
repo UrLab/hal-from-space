4cc7628722b54e87e8a8921b4a7c3e58d10dbd54
hyperflow <fhennecker@me.com>
2014-10-03 21:11:42 +0200

    [enh] testing on one actual HAL value

--- flask/bowman.py
+++ flask/bowman.py
@@ -2,18 +2,21 @@
 
 from flask import Flask, render_template
 from flask.ext.socketio import SocketIO, emit
+from halpy.hal import HAL
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'BITEBITEBITEBITEBITEBITEBITEB1TEBITE'
 app.debug = True
 socketio = SocketIO(app)
+hal = HAL("/dev/hal")
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 def emit_data_update():
-    emit('data_update', {'data':int(open("value").read().strip())}, broadcast=True)
+    print "Emitting", hal.read("animations/red/play").strip()
+    emit('data_update', {'data':int(hal.read("animations/red/play").strip())}, broadcast=True)
 
 @socketio.on('client_connected')
 def client_connected():
@@ -26,12 +29,16 @@ def ask_update(message):
 
 @socketio.on('value_switch_off')
 def value_switch_off(message):
-    open("value", 'w').write(str(0))
+    print "Asked for value switch off"
+    #open("value", 'w').write(str(0))
+    hal.stop("red")
     emit_data_update()
 
 @socketio.on('value_switch_on')
 def value_switch_on(message):
-    open("value", 'w').write(str(1))
+    print "Asked for value switch on"
+    #open("value", 'w').write(str(1))
+    hal.play("red")
     emit_data_update()
 
 if __name__ == '__main__':
