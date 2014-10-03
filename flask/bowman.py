8d6081b9f6ac2e9e130904eca73174fdd5867bc7
Florentin Hennecker <florentin.hennecker@gmail.com>
2014-10-03 08:56:18 +0200

    [add] working switch controller! shows the value of the switch, provides a button to change it

--- flask/bowman.py
+++ flask/bowman.py
@@ -12,14 +12,26 @@ socketio = SocketIO(app)
 def index():
     return render_template('index.html')
 
+def emit_data_update():
+    emit('data_update', {'data':int(open("value").read().strip())}, broadcast=True)
+
 @socketio.on('client_connected')
 def client_connected():
-	print "Client connected"
+    print "Client connected"
 
 @socketio.on('ask_update')
-def test_message(message):
+def ask_update(message):
+    emit_data_update()
 
-    emit('data_update', {'data':int(open("value").read().strip())}, broadcast=True)
+@socketio.on('value_switch_off')
+def value_switch_off(message):
+    open("value", 'w').write(str(0))
+    emit_data_update()
+
+@socketio.on('value_switch_on')
+def value_switch_on(message):
+    open("value", 'w').write(str(1))
+    emit_data_update()
 
 if __name__ == '__main__':
     socketio.run(app)
