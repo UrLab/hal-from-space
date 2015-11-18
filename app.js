9d52d0686282673232b4431275776032a284cbb9
iTitou <moiandme@gmail.com>
2015-11-18 19:22:02 +0100

    [enh] Refactor binary buttons and add mo4r swag

--- app.js
+++ app.js
@@ -1,29 +1,14 @@
-var Trigger = React.createClass({
-    render: function(){
-        return <h2>
-            <div className={"label label-" + this.state.button_class}>
-                {this.props.name}
-            </div>
-        </h2>
-    },
-    getInitialState: function(){
-        return {button_class: "default"};
-    },
-    onUpdate: function(res){
-        if (res[0] == true){
-            this.setState({button_class: "success"});
-        } else if (res[0] == false){
-            this.setState({button_class: "danger"});
-        } else {
-            this.setState({button_class: "default"});
-        }
-    },
-    componentDidMount: function(){
-        this.props.session.subscribe('trigger.'+this.props.name, this.onUpdate);
+var bool2class = function(aBoolean){
+    if (aBoolean == true){
+        return "success";
+    } else if (aBoolean == false){
+        return "danger";
     }
-});
+    return "default";
+};
 
 var Sensor = React.createClass({
+    halKey: function(){return 'sensor.' + this.props.name;},
     render: function(){
         var style = {'width': this.state.value + '%'};
         return <div className="progress">
@@ -39,33 +24,52 @@ var Sensor = React.createClass({
         this.setState({value: Math.ceil(100*res[0])});
     },
     componentDidMount: function(){
-        this.props.session.subscribe('sensor.'+this.props.name, this.onUpdate);
+        this.props.session.subscribe(this.halKey(), this.onUpdate);
     }
 });
 
-var Switch = React.createClass({
+/* <Binary session= prefix= suffix= name= writeable={true|false}> */
+var Binary = React.createClass({
+    halKey: function(){
+        var suffix = (this.props.suffix) ? ('.' + this.props.suffix) : '';
+        return this.props.prefix + '.' + this.props.name + suffix;
+    },
+    bootstrapClass: function(){
+        return (this.props.writeable ? "btn btn-" : "disabled btn btn-");
+    },
     render: function(){
-        return <button onClick={this.onClick} className={"btn btn-" + this.state.button_class}>
-            {this.props.name}
-        </button>
+        var klass = this.bootstrapClass() + this.state.button_class;
+        return <div onClick={this.onClick} className={klass}>
+            {this.props.name} {this.props.suffix}
+        </div>
     },
     getInitialState: function(){
         return {button_class: "default"};
     },
     onClick: function(ev){
-        this.props.session.call('switch.'+this.props.name+'.toggle');
-    },
-    onUpdate: function(res){
-        if (res[0] == true){
-            this.setState({button_class: "success"});
-        } else if (res[0] == false){
-            this.setState({button_class: "danger"});
-        } else {
-            this.setState({button_class: "default"});
+        if (this.props.writeable){
+            this.props.session.call(this.halKey() + '.toggle');
         }
     },
+    onUpdate: function(res){
+        this.setState({button_class: bool2class(res[0])});
+    },
     componentDidMount: function(){
-        this.props.session.subscribe('switch.'+this.props.name, this.onUpdate);
+        this.props.session.call(this.halKey() + '.state').then(function(res){
+            this.onUpdate([res]);
+        }.bind(this));
+        this.props.session.subscribe(this.halKey(), this.onUpdate);
+    }
+});
+
+var Animation = React.createClass({
+    render: function(){
+        return <div>
+            <Binary prefix="animation" suffix="play" writeable={true}
+                    name={this.props.name} session={this.props.session}/>
+            <Binary prefix="animation" suffix="loop" writeable={true}
+                    name={this.props.name} session={this.props.session}/>
+        </div>;
     }
 });
 
@@ -73,19 +77,24 @@ var HAL = React.createClass({
     render: function(){
         var session = this.props.session;
         var triggers = this.state.triggers.map(function(trig){
-            return <li><Trigger name={trig} session={session}/></li>
+            return <li><Binary prefix="trigger" name={trig} session={session}/></li>
         });
         var sensors = this.state.sensors.map(function(sens){
             return <li><Sensor name={sens} session={session}/></li>
         });
         var switchs = this.state.switchs.map(function(sw){
-            return <li><Switch name={sw} session={session}/></li>
+            return <li><Binary prefix="switch" name={sw}
+                               session={session} writeable={true}/></li>
+        });
+        var animations = this.state.animations.map(function(anim){
+            return <li><Animation name={anim} session={session}/></li>
         });
 
         return <div className="row">
             <ul className="col-md-3">{triggers}</ul>
             <ul className="col-md-3">{sensors}</ul>
             <ul className="col-md-3">{switchs}</ul>
+            <ul className="col-md-3">{animations}</ul>
         </div>
     },
     getInitialState: function(){
--- app.py
+++ app.py
@@ -26,26 +26,77 @@ class Component(ApplicationSession):
                 self.publish('sensor.'+sensor.name, sensor.value)
             yield from asyncio.sleep(2.5)
 
-    @asyncio.coroutine
-    def onJoin(self, details):
-        self.hal = WebHAL(HALFS_ROOT)
-        self.hal.install_loop()
-
+    def register_triggers(self):
         @self.hal.on_trigger()
         def publish_trigger(name, state):
             key = 'trigger.%s' % name
             self.publish(key, state)
 
-        yield from self.register(self.hal.layout, u'tree')
+        for trig in self.hal.triggers.values():
+            key = 'trigger.%s.state' % trig.name
+
+            def current_state_trigger(trigger=trig):
+                return trigger.on
+            yield from self.register(current_state_trigger, key)
+
+    def register_switchs(self):
         for sw in self.hal.switchs.values():
             key = 'switch.%s' % sw.name
-            def toggle_switch():
-                sw.on = not sw.on
-            yield from self.register(toggle_switch, key + '.toggle')
-            @sw.on_change
-            def publish_switch(*args):
-                self.publish(key, sw.on)
 
+            def current_state_switch(switch=sw):
+                return switch.on
+            yield from self.register(current_state_switch, key + '.state')
+
+            def toggle_switch(switch=sw):
+                switch.on = not switch.on
+            yield from self.register(toggle_switch, key + '.toggle')
+
+            @sw.on_change
+            def publish_switch(switch):
+                self.publish(key, switch.on)
+
+    def register_animations(self):
+        for anim in self.hal.animations.values():
+            key = 'animation.%s' % anim.name
+
+            def current_state_playing(animation=anim):
+                return animation.playing
+            yield from self.register(current_state_playing, key + '.play.state')
+
+            def toggle_playing(animation=anim):
+                animation.playing = not animation.playing
+            yield from self.register(toggle_playing, key + '.play.toggle')
+
+            def current_state_looping(animation=anim):
+                return animation.looping
+            yield from self.register(current_state_looping, key + '.loop.state')
+
+            def toggle_looping(animation=anim):
+                animation.looping = not animation.looping
+            yield from self.register(toggle_looping, key + '.loop.toggle')
+
+            def current_fps(animation=anim):
+                return animation.fps
+            yield from self.register(current_fps, key + '.fps.state')
+
+            def set_fps(animation=anim, fps):
+                animation.fps = int(fps)
+            yield from self.register(set_fps, key + '.fps.set')
+
+            @anim.on_change
+            def publish_anim(animation):
+                self.publish(key+'.loop', animation.looping)
+                self.publish(key+'.play', animation.playing)
+                self.publish(key+'.fps', animation.fps)
+
+    @asyncio.coroutine
+    def onJoin(self, details):
+        self.hal = WebHAL(HALFS_ROOT)
+        yield from self.register(self.hal.layout, u'tree')
+        yield from self.register_switchs()
+        yield from self.register_triggers()
+        yield from self.register_animations()
+        self.hal.install_loop()
         yield from self.periodic_tasks()
 
 
